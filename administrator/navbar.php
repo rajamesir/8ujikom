20bd101006fb1f13792616acafea5e06710a9c2c
chrisaldiba <danilbawole16@gmail.com>
2024-03-04 21:50:53 +0700

    upload

---
+++ administrator/navbar.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+
+<div class="card ">
+<div class="btn btn-secondary card-body text-white">
+		<h4>ADMIN</h4>
+		<a href="index.php" class="btn btn-secondary"> Beranda</a>
+		<a href="data_barang.php" class="btn btn-secondary"> Data Barang</a>
+		<a href="pembelian.php" class="btn btn-secondary"> transaksi</a>
+		<!--<a href="stok_barang.php" class="btn btn-success">Stok Barang</a>-->
+		<a href="data_pengguna.php" class="btn btn-secondary"> Data Pengguna</a>
+		<a href="../logout.php" class="btn btn-danger"> keluar</a>
+	</div>
+</div>
---
+++ petugas/stok_barang.php
@@ -0,0 +1,14 @@
+<?php
+include "header.php";
+include "navbar.php";
+?>
+<div class="card mt-2">
+	<div class="card-body">
+		<p>Selamat datang dihalaman administrator, silahkan anda bisa mengakses beberapa fitur</p>
+	</div>
+</div>
+<br>
+<br>
+<?php
+include "footer.php";
+?>
